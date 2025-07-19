4bffc91c2291dcd127815e894d825cd5d1aed142
t7o <theaveas.so@gmail.com>
2025-07-19 18:23:03 +0700

    add structure buffer

--- resources/shaders/SceneShader.hlsl
+++ resources/shaders/SceneShader.hlsl
@@ -1,22 +1,25 @@
+#define MAX_ENTITIES
 cbuffer Scene_CBuffer : register(b0) {
-    float4x4 world;
     float4x4 transform;
 };
 
 struct VS_Input {
-    float3 position: POSITION;
-    float4 color:    COLOR;
+    float3 position:   POSITION;
+    float4 color:      COLOR;
+    uint   instance_id: SV_InstanceID;
 };
 
 struct PS_Input {
-    float4 position: SV_POSITION;
-    float4 color:    COLOR;
+    float4 position:    SV_POSITION;
+    float4 color:       COLOR;
 };
+
+StructuredBuffer<matrix> world_matrices: register(t0);
 
 PS_Input VSMain(VS_Input vin) {
 
     PS_Input result;
-    result.position = mul(float4(vin.position, 1.0f), world);
+    result.position = mul(float4(vin.position, 1.0f), world_matrices[vin.instance_id]);
     result.position = mul(result.position, transform);
     result.color    = vin.color;
     return result;
